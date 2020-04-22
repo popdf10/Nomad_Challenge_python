c067dd568f70582382cb524710a5551dd7281dbb
popdf10 <popdf10@gmail.com>
2020-04-22 17:50:52 +0900

    hacker news scarapper

--- Day9-10/Day10.py
+++ Day9-10/Day10.py
@@ -1,6 +1,9 @@
 import requests
 import json
+import os
 from flask import Flask, render_template, request
+
+os.system('cls')
 
 db = {}
 popular_list = []
@@ -13,6 +16,8 @@ new_url = f"{base_url}/search_by_date?tags=story"
 
 # This URL gets the most popular stories
 popular_url = f"{base_url}/search?tags=story"
+
+item_url = f"{base_url}/items/"
 
 
 # This function makes the URL to get the detail of a storie by id.
@@ -38,8 +43,9 @@ def extract_list(db):
         author = popular.get('author')
         points = popular.get('points')
         num_comments = popular.get('num_comments')
+        objectID = popular.get('objectID')
         popular_list.append({'title': title, 'url': url, 'author': author,
-                             'points': points, 'num_comments': num_comments})
+                             'points': points, 'num_comments': num_comments, 'objectID': objectID})
 
     for new in db.get('new'):
         title = new.get('title')
@@ -47,11 +53,14 @@ def extract_list(db):
         author = new.get('author')
         points = new.get('points')
         num_comments = new.get('num_comments')
+        objectID = new.get('objectID')
         new_list.append({'title': title, 'url': url, 'author': author,
-                         'points': points, 'num_comments': num_comments})
+                         'points': points, 'num_comments': num_comments, 'objectID': objectID})
 
 
 extract_list(db)
+
+new_n_pop = {'new': new_list, 'popular': popular_list}
 
 app = Flask("DayNine")
 
@@ -66,9 +75,24 @@ def home():
     return render_template("index.html", popular_list=popular_list)
 
 
-@app.route("/detail")
-def detail():
-    return render_template("detail.html")
+@app.route("/<objectID>")
+def detail(objectID):
+    comment_list = []
+    for select in new_n_pop.values():
+        for item in select:
+            ID = item.get('objectID')
+            if ID == objectID:
+                comment = requests.get(f"{item_url}/{ID}").text
+                parsed_comment = json.loads(comment)['children']
+                for comment in parsed_comment:
+                    author = comment.get('author')
+                    text = comment.get('text')
+                    comment_list.append({'author': author, 'text': text})
+                if comment_list == []:
+                    comment_list.append(
+                        {'author': 'Thear is no comment', 'text': ''})
+
+    return render_template("detail.html", comment_list=comment_list)
 
 
 app.run(host="127.0.0.1")
