1ad8bff742458926a2c4e30e3584590e0a118bdb
popdf10 <popdf10@gmail.com>
2020-04-26 16:16:23 +0900

    initial commit

---
+++ Day13-14/Day13.py
@@ -0,0 +1,15 @@
+"""
+These are the URLs that will give you remote jobs for the word 'python'
+
+https://stackoverflow.com/jobs?r=true&q=python
+https://weworkremotely.com/remote-jobs/search?term=python
+https://remoteok.io/remote-dev+python-jobs
+
+Good luck!
+"""
+
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, render_template, request
+
+kjasldkjflsa
